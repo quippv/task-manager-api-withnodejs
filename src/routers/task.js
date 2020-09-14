e8b5b7b569be14804789eb709360c46e0a980f8f
@muhammadyusufadip.1999 <muhammadyusufadip.1999gmail.com>
2020-09-14 16:51:01 +0700

    Testing with express and database with jest

--- src/routers/task.js
+++ src/routers/task.js
@@ -95,7 +95,7 @@ router.patch("/tasks/:id", auth, async (req, res) => {
 
 router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete({
+    const task = await Task.findOneAndDelete({
       _id: req.params.id,
       owner: req.user._id,
     });
